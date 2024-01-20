d3eede47cc2b4349fa7f29424429cb9ebe908a8b
Kanchanpingale <kanchan14pingale@gmail.com>
2024-01-20 19:35:52 +0530

    first commit

---
+++ tesla-configurator-main/src/app/formdata.model.ts
@@ -0,0 +1,10 @@
+export interface FormData {
+    selectedOption1: object;
+    selectedColor: object;
+    selectedCode: object;
+    codeDetails: object;
+    selectedConfig:object;
+    selectedTowHitch:object;
+    
+
+  }
